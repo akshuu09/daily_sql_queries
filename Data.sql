4fd0c7ff49e26168d759404052e23611139c4f69
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-07-06 14:01:56 +0530

    Add files via upload

---
+++ Data.sql
@@ -0,0 +1,29 @@
+create database cybersuccess;
+use cybersuccess;
+show tables;
+select * from student;
+select * from Employee;
+select * from College;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
